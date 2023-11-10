b3744b5367f898643eef6f8f24acb6656b97bcd2
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-11-10 09:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let cont=document.createElement("div");
+cont.setAttribute("class", "container");
+for(let i=0;i<800;i++){
+	let squar=document.createElement("div");
+	squar.setAttribute("class", "square");
+	cont.appendChild(square);
+}
